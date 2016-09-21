3e71d21d6f23cf782c557e8e25e5b7ae61298b6b
Robin van Leeuwen <0845855@hr.nl>
2016-09-21 21:41:42 +0200

    Week 4
    Laravel bestanden. Gewerkt aan HTML + CSS homepage

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,159 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome!
+@endsection
+
+@section('content')
+    <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+            <div class="blog-post">
+                <h2 class="blog-post-title">Games with Gold in oktober 2016 bevat o.a. The Escapists en I Am Alive</h2>
+                <p class="blog-post-meta">21 september 2016 door <a href="#">Robin van Leeuwen</a></p>
+
+                <p>Hoewel het nog zo'n anderhalve week duurt voordat de maand oktober van start gaat, heeft Microsoft zojuist alvast onthuld welke gratis games Xbox Live Gold-abonnees volgende maand mogen verwachten. Zoals gebruikelijk zijn dit weer twee Xbox One- en twee Xbox 360-games.</p>
+
+                <p>Gedurende de gehele maand oktober kunnen Xbox One-gamers met een Gold-abonnement Super Mega Baseball: Extra Innings gratis aan hun catalogus toevoegen. Tussen 16 oktober en 15 november kunnen zij daarnaast ook The Escapists downloaden.</p>
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+                <h2 class="blog-post-title">Games with Gold in oktober 2016 bevat o.a. The Escapists en I Am Alive</h2>
+                <p class="blog-post-meta">21 september 2016 door <a href="#">Robin van Leeuwen</a></p>
+
+                <p>Hoewel het nog zo'n anderhalve week duurt voordat de maand oktober van start gaat, heeft Microsoft zojuist alvast onthuld welke gratis games Xbox Live Gold-abonnees volgende maand mogen verwachten. Zoals gebruikelijk zijn dit weer twee Xbox One- en twee Xbox 360-games.</p>
+
+                <p>Gedurende de gehele maand oktober kunnen Xbox One-gamers met een Gold-abonnement Super Mega Baseball: Extra Innings gratis aan hun catalogus toevoegen. Tussen 16 oktober en 15 november kunnen zij daarnaast ook The Escapists downloaden.</p>
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+                <h2 class="blog-post-title">Games with Gold in oktober 2016 bevat o.a. The Escapists en I Am Alive</h2>
+                <p class="blog-post-meta">21 september 2016 door <a href="#">Robin van Leeuwen</a></p>
+
+                <p>Hoewel het nog zo'n anderhalve week duurt voordat de maand oktober van start gaat, heeft Microsoft zojuist alvast onthuld welke gratis games Xbox Live Gold-abonnees volgende maand mogen verwachten. Zoals gebruikelijk zijn dit weer twee Xbox One- en twee Xbox 360-games.</p>
+
+                <p>Gedurende de gehele maand oktober kunnen Xbox One-gamers met een Gold-abonnement Super Mega Baseball: Extra Innings gratis aan hun catalogus toevoegen. Tussen 16 oktober en 15 november kunnen zij daarnaast ook The Escapists downloaden.</p>
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+                <h2 class="blog-post-title">Games with Gold in oktober 2016 bevat o.a. The Escapists en I Am Alive</h2>
+                <p class="blog-post-meta">21 september 2016 door <a href="#">Robin van Leeuwen</a></p>
+
+                <p>Hoewel het nog zo'n anderhalve week duurt voordat de maand oktober van start gaat, heeft Microsoft zojuist alvast onthuld welke gratis games Xbox Live Gold-abonnees volgende maand mogen verwachten. Zoals gebruikelijk zijn dit weer twee Xbox One- en twee Xbox 360-games.</p>
+
+                <p>Gedurende de gehele maand oktober kunnen Xbox One-gamers met een Gold-abonnement Super Mega Baseball: Extra Innings gratis aan hun catalogus toevoegen. Tussen 16 oktober en 15 november kunnen zij daarnaast ook The Escapists downloaden.</p>
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+                <h2 class="blog-post-title">Games with Gold in oktober 2016 bevat o.a. The Escapists en I Am Alive</h2>
+                <p class="blog-post-meta">21 september 2016 door <a href="#">Robin van Leeuwen</a></p>
+
+                <p>Hoewel het nog zo'n anderhalve week duurt voordat de maand oktober van start gaat, heeft Microsoft zojuist alvast onthuld welke gratis games Xbox Live Gold-abonnees volgende maand mogen verwachten. Zoals gebruikelijk zijn dit weer twee Xbox One- en twee Xbox 360-games.</p>
+
+                <p>Gedurende de gehele maand oktober kunnen Xbox One-gamers met een Gold-abonnement Super Mega Baseball: Extra Innings gratis aan hun catalogus toevoegen. Tussen 16 oktober en 15 november kunnen zij daarnaast ook The Escapists downloaden.</p>
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+                <h2 class="blog-post-title">Games with Gold in oktober 2016 bevat o.a. The Escapists en I Am Alive</h2>
+                <p class="blog-post-meta">21 september 2016 door <a href="#">Robin van Leeuwen</a></p>
+
+                <p>Hoewel het nog zo'n anderhalve week duurt voordat de maand oktober van start gaat, heeft Microsoft zojuist alvast onthuld welke gratis games Xbox Live Gold-abonnees volgende maand mogen verwachten. Zoals gebruikelijk zijn dit weer twee Xbox One- en twee Xbox 360-games.</p>
+
+                <p>Gedurende de gehele maand oktober kunnen Xbox One-gamers met een Gold-abonnement Super Mega Baseball: Extra Innings gratis aan hun catalogus toevoegen. Tussen 16 oktober en 15 november kunnen zij daarnaast ook The Escapists downloaden.</p>
+
+            </div><!-- /.blog-post -->
+
+            <nav>
+                <ul class="pager">
+                    <li><a href="#">Previous</a></li>
+                    <li><a href="#">Next</a></li>
+                </ul>
+            </nav>
+
+        </div><!-- /.blog-main -->
+
+        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+            <div class="sidebar-module sidebar-module-inset" style="border: 1px solid black">
+                <h3>Inloggen</h3>
+                <form action="#" method="post">
+                    <div class="form-group">
+                        <label for="email">E-mailadres</label>
+                        <input class="form-control" type="text" name="email" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Wachtwoord</label>
+                        <input class="form-control" type="password" name="password" id="password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Inloggen</button>
+                </form>
+            </div>
+            <div class="sidebar-module">
+                <h4>Archives</h4>
+                <ol class="list-unstyled">
+                    <li><a href="#">March 2014</a></li>
+                    <li><a href="#">February 2014</a></li>
+                    <li><a href="#">January 2014</a></li>
+                    <li><a href="#">December 2013</a></li>
+                    <li><a href="#">November 2013</a></li>
+                    <li><a href="#">October 2013</a></li>
+                    <li><a href="#">September 2013</a></li>
+                    <li><a href="#">August 2013</a></li>
+                    <li><a href="#">July 2013</a></li>
+                    <li><a href="#">June 2013</a></li>
+                    <li><a href="#">May 2013</a></li>
+                    <li><a href="#">April 2013</a></li>
+                </ol>
+            </div>
+            <div class="sidebar-module">
+                <h4>Elsewhere</h4>
+                <ol class="list-unstyled">
+                    <li><a href="#">GitHub</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Facebook</a></li>
+                </ol>
+            </div>
+        </div><!-- /.blog-sidebar -->
+
+    </div><!-- /.row -->
+
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Sign Up</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="first-name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-md-6">
+            <h3>Inloggen</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">E-mailadres</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Wachtwoord</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Inloggen</button>
+            </form>
+        </div>
+    </div>
+@endsection
